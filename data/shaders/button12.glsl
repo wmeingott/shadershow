5aab64cf6efe12d7e5cb153c2d371abb3831489a
christoph <zapod@cg.clayhill.de>
2026-02-08 23:14:22 +0100

    button12.glsl

--- data/shaders/button12.glsl
+++ data/shaders/button12.glsl
@@ -1,14 +1,22 @@
 // @param cColor color [1.0, 0.3, 0.1] "Fire color"
-// @param rradius float 0.5 [0.0, 1.0] "radius"
+// @param rradius float 0.5 [0.0, 1.0] "radius halo"
+// @param mradius float 0.1 [0.0, 1.0] "Radius voll"
+// @param gridsize int 10 [0,100] "Anzahl"
+// @param speedx float 0.0 [-2.0,2.0] "Geschwindigkeit X"
+// @param speedy float 0.0 [-2.0,2.0] "Geschwindigkeit Y"
+
 
 
 void mainImage(out vec4 fragColor, in vec2 fragCoord) {
     // Normalize coordinates to 0-1 range
     vec2 uv = fragCoord.xy / iResolution.xy;
     
+    uv.x = uv.x + (mod(iTime, iResolution.x) * speedx);
+    uv.y = uv.y + (mod(iTime, iResolution.y) * speedy);
+    
     // Grid parameters
-    float gridSize = 10.0;
-    float radius = rradius * 0.1;
+    float gridSize = float(gridsize);
+    float radius = rradius * (1.0 / gridSize);
     
     // Scale UV to grid space (0 to 10)
     vec2 gridUV = uv * gridSize;
@@ -25,7 +33,7 @@ void mainImage(out vec4 fragColor, in vec2 fragCoord) {
     float dist = distance(cellUV, vec2(0.5));
     
     // Draw circle: white inside, black outside
-    float circle = step(cellRadius, dist);
+    float circle = smoothstep(mradius, cellRadius, dist);
     
     // Output color
     vec3 color = cColor - vec3(circle);
